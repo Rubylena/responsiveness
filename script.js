ac86fc76fbf889510e63aa9609ce830a6a19981a
[Ruby] <graceffiong@gmail.com>
2021-04-10 19:34:32 +0100

    responsive hamburger menu and nav bar added

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function menuBar(){
   const bar = document.getElementById("nav-link");
+  const menu = document.querySelector("menu-bar");
   if (bar.className === "nav-links") {
-      bar.className += "menu-bar";
-      
+    bar.className += menu;  
   } else{
-      bar.className = "nav-links";
-      
+    bar.className = "nav-links";  
+
   }
 }
 
